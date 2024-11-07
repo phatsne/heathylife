9f841867c6e176cc070a99afd95cbac0cd082ab4
Phat <trieucuongphat@gmail.com>
2024-11-07 22:56:15 +0700

    update button

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -1,65 +1,64 @@
 import React from "react";
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
+import About1 from "../../Assets/Images/about.jpg"
 
 import './about.css';
 
 const About = () => {
   return(
-    <Container fluid className="py-5" style={{ backgroundColor: '#f7f3ee', }}>
-      <Row>
+    <Container fluid className="py-5" style={{ backgroundColor: '#ebfff7', }}>
+      <Row className="m-2 px-4">
         <Col md={6} className="d-flex align-items-center">
-          <img
-            src="https://via.placeholder.com/400"
-            alt="Yoga"
-            className="img-fluid rounded"
-          />
+          <div className="">
+            <Card.Img variant="top" src={About1} alt="About1" style={{ borderRadius: '10px' }}/> 
+          </div>
         </Col>
         <Col md={6}>
           <h3 className="fw-bold mb-3" style={{ color: '#333' }}>Đôi Nét Về Heathy Life</h3>
-            <p>
-              Heathy Life là trang web tổng hợp kiến thức yoga từ cơ bản đến nâng cao dành cho những bạn
-              muốn tập yoga tại nhà!
-            </p>
-            <p>
-              Với niềm tin rằng mọi người, dù bận rộn đến đâu, cũng xứng đáng được chăm sóc sức khỏe
-              và tinh thần của mình.
-            </p>
-            <p>
-              Heathy Life mang đến một giải pháp linh hoạt, giúp yoga trở nên dễ tiếp cận hơn bao giờ hết,
-              đồng thời xây dựng một cộng đồng tích cực, nơi mọi người có thể cùng nhau cải thiện sức
-              khỏe và tìm kiếm cân bằng trong cuộc sống.
-            </p>
-            <Row className="mt-4">
-              <Col xs={6}>
-                <div className="d-flex align-items-center mb-3">
-                  <i className="bi bi-journal-check me-2" style={{ fontSize: '1.2rem', color: '#333' }}></i>
-                  <p className="mb-0">Kiến thức Yoga chuẩn</p>
-                </div>
-                <div className="d-flex align-items-center mb-3">
-                  <i className="bi bi-clock-history me-2" style={{ fontSize: '1.2rem', color: '#333' }}></i>
-                  <p className="mb-0">Thời gian linh hoạt</p>
-                </div>
-                <div className="d-flex align-items-center mb-3">
-                  <i className="bi bi-check2-square me-2" style={{ fontSize: '1.2rem', color: '#333' }}></i>
-                  <p className="mb-0">Xây dựng thói quen tốt</p>
-                </div>
-              </Col>
-              <Col xs={6}>
-                <div className="d-flex align-items-center mb-3">
-                  <i className="bi bi-wallet2 me-2" style={{ fontSize: '1.2rem', color: '#333' }}></i>
-                  <p className="mb-0">Tiết kiệm chi phí</p>
-                </div>
-                <div className="d-flex align-items-center mb-3">
-                  <i className="bi bi-house-door me-2" style={{ fontSize: '1.2rem', color: '#333' }}></i>
-                  <p className="mb-0">Không gian thoải mái</p>
-                </div>
-                <div className="d-flex align-items-center mb-3">
-                  <i className="bi bi-heart-pulse me-2" style={{ fontSize: '1.2rem', color: '#333' }}></i>
-                  <p className="mb-0">Tăng cường sức khỏe</p>
-                </div>
-              </Col>
-            </Row>
-              <Button variant="danger" className="mt-3">Xem Thêm Về Chúng Tôi</Button>
+          <p style={{ textAlign: 'left' }}>
+            Heathy Life là trang web tổng hợp kiến thức yoga từ cơ bản đến nâng cao dành cho những bạn
+            muốn tập yoga tại nhà!
+          </p>
+          <p style={{ textAlign: 'left' }}>
+            Với niềm tin rằng mọi người, dù bận rộn đến đâu, cũng xứng đáng được chăm sóc sức khỏe
+            và tinh thần của mình.
+          </p>
+          <p style={{ textAlign: 'left' }}>
+            Heathy Life mang đến một giải pháp linh hoạt, giúp yoga trở nên dễ tiếp cận hơn bao giờ hết,
+            đồng thời xây dựng một cộng đồng tích cực, nơi mọi người có thể cùng nhau cải thiện sức
+            khỏe và tìm kiếm cân bằng trong cuộc sống.
+          </p>
+          <Row className="mt-4" >
+            <Col xs={6} className="px-3">
+              <div className="d-flex align-items-center mb-3">
+                <i className="bi bi-journal-check me-2" style={{ fontSize: '1.2rem', color: '#198754' }}></i>
+                <p className="mb-0">Kiến thức Yoga chuẩn</p>
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <i className="bi bi-clock-history me-2" style={{ fontSize: '1.2rem', color: '#198754' }}></i>
+                <p className="mb-0">Thời gian linh hoạt</p>
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <i className="bi bi-check2-square me-2" style={{ fontSize: '1.2rem', color: '#198754' }}></i>
+                <p className="mb-0">Xây dựng thói quen tốt</p>
+              </div>
+            </Col>
+            <Col xs={6} className="px-3">
+              <div className="d-flex align-items-center mb-3">
+                <i className="bi bi-wallet2 me-2" style={{ fontSize: '1.2rem', color: '#198754' }}></i>
+                <p className="mb-0">Tiết kiệm chi phí</p>
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <i className="bi bi-house-door me-2" style={{ fontSize: '1.2rem', color: '#198754' }}></i>
+                <p className="mb-0">Không gian thoải mái</p>
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <i className="bi bi-heart-pulse me-2" style={{ fontSize: '1.2rem', color: '#198754' }}></i>
+                <p className="mb-0">Tăng cường sức khỏe</p>
+              </div>
+            </Col>
+          </Row>
+          <Button variant="success" className="mt-3">Xem Thêm Về Chúng Tôi</Button>
         </Col>
       </Row>
     </Container>
--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import './hero.css'
+import Hero1 from '../../Assets/Images/hero1.jpg';
+import Hero2 from '../../Assets/Images/hero2.jpg';
+import Hero3 from '../../Assets/Images/hero3.jpg';
 
 const HeroSection = () => {
   return (
@@ -23,39 +26,39 @@ const HeroSection = () => {
         <Row className="gy-4">
             <Col md={4}>
             <Card>
-                <Card.Img variant="top" src="https://via.placeholder.com/300x200" alt="Yoga 1" />
+                <Card.Img variant="top" src={Hero1}alt="Hero1" />
                 <Card.Body>
                 <Card.Title>Tập Yoga Cơ Bản Ngay Tại Nhà Với Nguyễn Hiếu</Card.Title>
                 <Card.Text>
                     Khóa học được thiết kế dành cho người mới bắt đầu. Bắt đầu hành trình thay đổi bản thân với những tư thế đơn giản, không gây nguy hiểm nhưng mang lại hiệu quả tuyệt vời.
                 </Card.Text>
-                <Button variant="danger">Tìm Hiểu Thêm</Button>
+                <Button variant="success">Tìm Hiểu Thêm</Button>
                 </Card.Body>
             </Card>
             </Col>
             
             <Col md={4}>
             <Card>
-                <Card.Img variant="top" src="https://via.placeholder.com/300x200" alt="Yoga 2" />
+                <Card.Img variant="top" src={Hero2} alt="Hero2" />
                 <Card.Body>
                 <Card.Title>Yoga Giảm Eo, Giảm Mỡ Giữ Dáng Thon Gọn</Card.Title>
                 <Card.Text>
                     Các tư thế trong khóa học được thiết kế dành riêng cho những bạn muốn đánh tan mỡ bụng, lấy lại vóc dáng. Ngoài ra còn cung cấp các bài tập giúp tạo đường cong hoàn mỹ.
                 </Card.Text>
-                <Button variant="danger">Tìm Hiểu Thêm</Button>
+                <Button variant="success">Tìm Hiểu Thêm</Button>
                 </Card.Body>
             </Card>
             </Col>
             
             <Col md={4}>
             <Card>
-                <Card.Img variant="top" src="https://via.placeholder.com/300x200" alt="Yoga 3" />
+                <Card.Img variant="top" src={Hero3} alt="Hero3" />
                 <Card.Body>
                 <Card.Title>Yoga Trẻ Hóa Và Làm Đẹp Cho Khuôn Mặt</Card.Title>
                 <Card.Text>
                     Bạn không có nhiều thời gian chăm sóc sắc đẹp? Đừng lo lắng, vì khóa học này được thiết kế dành cho bạn. Với kĩ thuật đơn giản, an toàn, làn da của bạn sẽ trở nên săn mịn và tràn đầy sức sống.
                 </Card.Text>
-                <Button variant="danger">Tìm Hiểu Thêm</Button>
+                <Button variant="success">Tìm Hiểu Thêm</Button>
                 </Card.Body>
             </Card>
             </Col>
